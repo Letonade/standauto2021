8a0a258431d1e4c267e9d9949decf24b38a2d109
Letonade <cocoscorpio94@hotmail.fr>
2020-12-25 12:35:57 +0100

    Template de base en place
    Progressus

--- assets/app.js
+++ assets/app.js
@@ -2,4 +2,16 @@
 //assets/js/app.js
 import 'bootstrap';
 //fichier scss contenant nos css
-import './css/app.scss';
+//import './css/app.scss';
+
+import './styles/Progressus/css/bootstrap.min.css';
+import './styles/Progressus/css/font-awesome.min.css';
+import './styles/Progressus/css/bootstrap-theme.css';
+import './styles/Progressus/css/main.css';
+
+import './styles/Progressus/js/google-map.js';
+import './styles/Progressus/js/headroom.min.js';
+import './styles/Progressus/js/html5shiv.js';
+import './styles/Progressus/js/jQuery.headroom.min.js';
+import './styles/Progressus/js/respond.min.js';
+import './styles/Progressus/js/template.js';
