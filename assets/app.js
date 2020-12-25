3b236494b66a00c3dfd8771c8910105ea0bdada9
Letonade <cocoscorpio94@hotmail.fr>
2020-12-25 14:52:11 +0100

    Think of it as a stable build

--- assets/app.js
+++ assets/app.js
@@ -9,9 +9,11 @@ import './styles/Progressus/css/font-awesome.min.css';
 import './styles/Progressus/css/bootstrap-theme.css';
 import './styles/Progressus/css/main.css';
 
-import './styles/Progressus/js/google-map.js';
-import './styles/Progressus/js/headroom.min.js';
+//import './styles/Progressus/js/extra/jquery.min.js';
+//import './styles/Progressus/js/extra/bootstrap.min.js';
+//import './styles/Progressus/js/google-map.js';
+//import './styles/Progressus/js/respond.min.js';
 import './styles/Progressus/js/html5shiv.js';
+import './styles/Progressus/js/headroom.min.js';
 import './styles/Progressus/js/jQuery.headroom.min.js';
-import './styles/Progressus/js/respond.min.js';
 import './styles/Progressus/js/template.js';
